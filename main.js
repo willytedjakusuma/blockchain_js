c6dc03f332f63fb7e4a49a4f65f5a271d6b74349
Willy Tedjakusuma <willy.tedjakusuma@gmail.com>
2020-05-20 19:53:50 +0700

    Initialize BlockChain Core Engine

---
+++ main.js
@@ -0,0 +1,63 @@
+const SHA256  = require('crypto-js/sha256');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash(); 
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();    
+  }
+}
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '12/06/1993', 'Adam', '0') 
+  }
+
+  getLatestBlock(){
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid(){
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+
+let myBlockChain = new BlockChain();
+myBlockChain.addBlock( new Block(1, Date.now(), { amount: 5 }))
+myBlockChain.addBlock( new Block(2, Date.now(), { amount: 3 }))
+
+// console.log(JSON.stringify(myBlockChain, null, 2));
+console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
+myBlockChain.chain[1].data = { amount: 500 }
+console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
+

ac7b94810bcbf828983f98f543ae398f9d23b331
Willy Tedjakusuma <willy.tedjakusuma@gmail.com>
2020-05-20 20:17:08 +0700

    Added Readme and Proof Of Work ( Mining )

---
+++ Readme.md
@@ -0,0 +1,16 @@
+Block Chain on JavaScript
+========
+
+**Requirement**
+
+This project is created with Node JS Version 12.16.3
+
+1. Install Crypto-JS 
+    ```
+    npm install --save crypto-js
+    ```
+
+2. Play the script
+    ```
+    node ./main.js
+    ```
--- main.js
+++ main.js
@@ -7,16 +7,28 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash(); 
+    this.randomInt = 0; // To Tamper data that not actually modifiying the data, so its a random integer. Aka Nonce
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();    
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.randomInt).toString();    
+  }
+
+  mineBlock(level) {
+    while(this.hash.substring(0, level) !== Array(level + 1).join('0')){
+      this.randomInt++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block has been mined: " + this.hash);
+    
   }
 }
 
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.level = 5;
   }
 
   createGenesisBlock() {
@@ -29,7 +41,8 @@ class BlockChain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.level);
     this.chain.push(newBlock);
   }
 
@@ -38,10 +51,12 @@ class BlockChain {
       const currentBlock = this.chain[i];
       const previousBlock = this.chain[i - 1];
 
+      // Make sure data is not tempered with comparing hash
       if (currentBlock.hash !== currentBlock.calculateHash()) {
         return false;
       }
-
+      
+      // Make sure data is not tempered with comparing with hash from previous Block
       if (currentBlock.previousHash !== previousBlock.hash) {
         return false;
       }
@@ -53,11 +68,14 @@ class BlockChain {
 
 
 let myBlockChain = new BlockChain();
+console.log('Mining first data ....')
 myBlockChain.addBlock( new Block(1, Date.now(), { amount: 5 }))
+console.log('Mining second data ....')
 myBlockChain.addBlock( new Block(2, Date.now(), { amount: 3 }))
 
-// console.log(JSON.stringify(myBlockChain, null, 2));
-console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
-myBlockChain.chain[1].data = { amount: 500 }
-console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
+// // console.log(JSON.stringify(myBlockChain, null, 2));
+// console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
+// myBlockChain.chain[1].data = { amount: 500 };
+// myBlockChain.chain[1].hash = myBlockChain.chain[1].calculateHash();
+// console.log('is my block chain valid ? ' + myBlockChain.isChainValid() + '\n')
 
